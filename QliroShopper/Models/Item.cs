3143b68f7b28395ebe9f6f3d885fd755e08eb9dd
Karl Johan Krantz <schwomp@gmail.com>
2017-04-17 17:14:53 +0200

    Stop serialization of Item->Order
    This causes loops in the serialization. These are bad.

--- QliroShopper/Models/Item.cs
+++ QliroShopper/Models/Item.cs
@@ -1,5 +1,6 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.Serialization;
 
 namespace QliroShopper.Models {
     public class Item {
@@ -9,6 +10,8 @@ namespace QliroShopper.Models {
         public string Description { get; set; }
         public decimal Price { get; set; }
         public int Quantity { get; set; }
+
+        [IgnoreDataMember]
         public virtual Order Order { get; set; }
     }
 }
